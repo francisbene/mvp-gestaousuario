a75e2abf72e202fdf192fec0bbdf911f3e4a38f3
Francisbene Mayer <fmmcba1@gmail.com>
2024-03-28 14:49:40 -0400

    projeto faltando banco dados

--- routes/usuario.py
+++ routes/usuario.py
@@ -30,25 +30,56 @@ def new_usuario():
     """ inserir novo usuário no formulário """
     return render_template('new_usuario.html')
 
+
 @usuario_route.route('/<int:usuario_id>')
-def info_usuario(cliente_id):
+def info_usuario(usuario_id):
     """ exibir informações do usuário """
-    return render_template('info_usuario.html')
+    
+    usuario = list(filter(lambda user: user['id'] == usuario_id, USUARIOS)) [0]
+    
+    return render_template('info_usuario.html', usuario=usuario)
+
 
 @usuario_route.route('/<int:usuario_id>/edit')
-def edit_usuario(cliente_id):
+def edit_usuario(usuario_id):
     """ formulário para editar um usuário """
-    return edit_usuario('edit_usuario.html')
+    
+    usuario = None
+    for user in USUARIOS:
+        if user['id'] == usuario_id:
+            usuario = user
+    
+    return render_template('new_usuario.html', usuario=usuario)
+
 
 @usuario_route.route('/<int:usuario_id>/update', methods=['PUT'])
-def atualizar_usuario(cliente_id):
+def atualizar_usuario(usuario_id):
     """ Atualizar informações de um usuário """
-    pass
+    usuario_atualizado = None
+    # obter dados do formulario de edicao
+    data = request.json
+    
+    # obter usuario pelo id
+    
+    for user in USUARIOS:
+        if user['id'] == usuario_id:
+            user['nome'] = data['nome']
+            user['email'] = data['email']
+            
+            usuario_atualizado  = user
+    
+    # editar usuario
+    return render_template('item_usuario.html', usuario=usuario_atualizado)
+    
+    
 
 @usuario_route.route('/<int:usuario_id>/delete', methods=['DELETE'])
-def deletar_usuario(cliente_id):
-    """ Deletar usuário """
-    pass
+def deletar_usuario(usuario_id):
+    global USUARIOS
+    USUARIOS = [ user for user in USUARIOS if user['id'] != usuario_id ]
+    
+    return {'deleted': 'ok'}
+    
 
     
 
