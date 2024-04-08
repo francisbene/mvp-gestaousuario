b15e63185b910298a899454b5498ba048449bd87
Francisbene Mayer <fmmcba1@gmail.com>
2024-04-08 17:14:58 -0300

    conexão com swagger

--- config.py
+++ config.py
@@ -2,13 +2,11 @@ from routes.home import home_route
 from routes.usuario import usuario_route
 from database.database import db
 from database.models.usuario import Usuario
-#from flasgger import Swagger
 
 
 def configure_all(app):
     configure_routes(app)
     configure_db()
-    # configure_swagger(app)
 
 
 def configure_routes(app):
@@ -19,6 +17,5 @@ def configure_db():
     db.connect()
     db.create_tables([Usuario])
 
-# def configure_swagger(app):
-#     swagger = Swagger(app, template_file='swagger.yml')
+
 
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flasgger import Swagger
 from config import configure_all
+# from routes.usuario import usuario_route
+# from routes.home import home_route
+
+# from models.user import db, User
 
 app = Flask(__name__)
 
+# app.register_blueprint(home_route)
+# app.register_blueprint(usuario_route)
+
+swagger = Swagger(app)
+
 configure_all(app)
 
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
