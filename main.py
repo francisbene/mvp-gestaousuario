de7aa1e4edae5e6b87e97995dbb121291ab9538c
Francisbene Mayer <fmmcba1@gmail.com>
2024-04-17 19:17:31 -0300

    gitignore

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flasgger import Swagger
+from flask_cors import CORS
 from config import configure_all
 
 app = Flask(__name__)
 
 swagger = Swagger(app)
+
+CORS(app)
 
 configure_all(app)
 
