436b83a6a3d0a3ddc329f2696c79638ccf3fc1e4
Francisbene Mayer <fmmcba1@gmail.com>
2024-04-17 12:05:36 -0400

    alterações

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from peewee import PostgresqlDatabase
 import psycopg2
 
 db = PostgresqlDatabase(database='usersmanager', user='postgres', host='localhost',
-                        password='pgadmin')
+                        password='postgres')
 
 
 # from peewee import SqliteDatabase
